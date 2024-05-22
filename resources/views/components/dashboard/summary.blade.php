75fc65cf988c594420538d9b4eb55742a5d42752
emran594 <emranhasans594@gmail.com>
2024-05-22 19:32:10 +0600

    ..

--- resources/views/components/dashboard/summary.blade.php
+++ resources/views/components/dashboard/summary.blade.php
@@ -1,163 +1,138 @@
 <div class="container-fluid">
     <div class="row">
-
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 animated fadeIn p-2">
-            <div class="card card-plain h-100 bg-white">
-                <div class="p-3">
-                    <div class="row">
-                        <div class="col-9 col-lg-8 col-md-8 col-sm-9">
-                            <div>
-                                <h5 class="mb-0 text-capitalize font-weight-bold">
-                                    <span id="product"></span>
-                                </h5>
-                                <p class="mb-0 text-sm">Product</p>
-                            </div>
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-2">
+            <div class="card card-custom h-100 bg-gradient-dark text-white">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h5 class="mb-0 font-weight-bold">
+                                <span id="product"></span>
+                            </h5>
+                            <p class="mb-0">Product</p>
                         </div>
-                        <div class="col-3 col-lg-4 col-md-4 col-sm-3 text-end">
-                            <div class="icon icon-shape bg-gradient-primary shadow float-end border-radius-md">
-                                <img class="w-100 " src="{{asset('images/icon.svg')}}"/>
+                        <div class="col-4 text-right">
+                            <div class="icon-custom">
+                                <img class="w-100" src="{{asset('images/product.svg')}}" alt="Product Icon"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 animated fadeIn p-2">
-            <div class="card card-plain h-100 bg-white">
-                <div class="p-3">
-                    <div class="row">
-                        <div class="col-9 col-lg-8 col-md-8 col-sm-9">
-                            <div>
-                                <h5 class="mb-0 text-capitalize font-weight-bold">
-                                    <span id="category"></span>
-                                </h5>
-                                <p class="mb-0 text-sm">Category</p>
-                            </div>
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-2">
+            <div class="card card-custom h-100 bg-gradient-success text-white">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h5 class="mb-0 font-weight-bold">
+                                <span id="category"></span>
+                            </h5>
+                            <p class="mb-0">Category</p>
                         </div>
-                        <div class="col-3 col-lg-4 col-md-4 col-sm-3 text-end">
-                            <div class="icon icon-shape bg-gradient-primary shadow float-end border-radius-md">
-                                <img class="w-100 " src="{{asset('images/icon.svg')}}"/>
+                        <div class="col-4 text-right">
+                            <div class="icon-custom">
+                                <img class="w-100" src="{{asset('images/category.svg')}}" alt="Category Icon"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 animated fadeIn p-2">
-            <div class="card card-plain h-100 bg-white">
-                <div class="p-3">
-                    <div class="row">
-                        <div class="col-9 col-lg-8 col-md-8 col-sm-9">
-                            <div>
-                                <h5 class="mb-0 text-capitalize font-weight-bold">
-                                    <span id="customer"></span>
-                                </h5>
-                                <p class="mb-0 text-sm">Customer</p>
-                            </div>
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-2">
+            <div class="card card-custom h-100 bg-gradient-dark text-white">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h5 class="mb-0 font-weight-bold">
+                                <span id="customer"></span>
+                            </h5>
+                            <p class="mb-0">Customer</p>
                         </div>
-                        <div class="col-3 col-lg-4 col-md-4 col-sm-3 text-end">
-                            <div class="icon icon-shape bg-gradient-primary shadow float-end border-radius-md">
-                                <img class="w-100 " src="{{asset('images/icon.svg')}}"/>
+                        <div class="col-4 text-right">
+                            <div class="icon-custom">
+                                <img class="w-100" src="{{asset('images/customer.svg')}}" alt="Customer Icon"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 animated fadeIn p-2">
-            <div class="card card-plain h-100  bg-white">
-                <div class="p-3">
-                    <div class="row">
-                        <div class="col-9 col-lg-8 col-md-8 col-sm-9">
-                            <div>
-                                <h5 class="mb-0 text-capitalize font-weight-bold">
-                                    <span id="invoice"></span>
-                                </h5>
-                                <p class="mb-0 text-sm">Invoice</p>
-                            </div>
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-2">
+            <div class="card card-custom h-100 bg-gradient-primary text-white">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h5 class="mb-0 font-weight-bold">
+                                <span id="invoice"></span>
+                            </h5>
+                            <p class="mb-0">Invoice</p>
                         </div>
-                        <div class="col-3 col-lg-4 col-md-4 col-sm-3 text-end">
-                            <div class="icon icon-shape bg-gradient-primary shadow float-end border-radius-md">
-                                <img class="w-100 " src="{{asset('images/icon.svg')}}"/>
+                        <div class="col-4 text-right">
+                            <div class="icon-custom">
+                                <img class="w-100" src="{{asset('images/invoice.svg')}}" alt="Invoice Icon"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 animated fadeIn p-2">
-            <div class="card card-plain h-100 bg-white">
-                <div class="p-3">
-                    <div class="row">
-                        <div class="col-9 col-lg-8 col-md-8 col-sm-9">
-                            <div>
-                                <h5 class="mb-0 text-capitalize font-weight-bold">
-                                    $ <span id="total"></span>
-                                </h5>
-                                <p class="mb-0 text-sm">Total Sale</p>
-                            </div>
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-2">
+            <div class="card card-custom h-100 bg-gradient-warning text-white">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h5 class="mb-0 font-weight-bold">
+                                $ <span id="total"></span>
+                            </h5>
+                            <p class="mb-0">Total Sale</p>
                         </div>
-                        <div class="col-3 col-lg-4 col-md-4 col-sm-3 text-end">
-                            <div class="icon icon-shape bg-gradient-primary shadow float-end border-radius-md">
-                                <img class="w-100 " src="{{asset('images/icon.svg')}}"/>
+                        <div class="col-4 text-right">
+                            <div class="icon-custom">
+                                <img class="w-100" src="{{asset('images/sale.svg')}}" alt="Total Sale Icon"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 animated fadeIn p-2">
-            <div class="card card-plain h-100  bg-white">
-                <div class="p-3">
-                    <div class="row">
-                        <div class="col-9 col-lg-8 col-md-8 col-sm-9">
-                            <div>
-                                <h5 class="mb-0 text-capitalize font-weight-bold">
-                                    $ <span id="vat"></span>
-                                </h5>
-                                <p class="mb-0 text-sm">Vat Collection</p>
-                            </div>
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-2">
+            <div class="card card-custom h-100 bg-gradient-dark text-white">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h5 class="mb-0 font-weight-bold">
+                                $ <span id="vat"></span>
+                            </h5>
+                            <p class="mb-0">Vat Collection</p>
                         </div>
-                        <div class="col-3 col-lg-4 col-md-4 col-sm-3 text-end">
-                            <div class="icon icon-shape bg-gradient-primary shadow float-end border-radius-md">
-                                <img class="w-100 " src="{{asset('images/icon.svg')}}"/>
+                        <div class="col-4 text-right">
+                            <div class="icon-custom">
+                                <img class="w-100" src="{{asset('images/tax.svg')}}" alt="Vat Collection Icon"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-
-        <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 animated fadeIn p-2">
-            <div class="card card-plain h-100  bg-white">
-                <div class="p-3">
-                    <div class="row">
-                        <div class="col-9 col-lg-8 col-md-8 col-sm-9">
-                            <div>
-                                <h5 class="mb-0 text-capitalize font-weight-bold">
-                                    $ <span id="payable"></span>
-                                </h5>
-                                <p class="mb-0 text-sm">Total Collection</p>
-                            </div>
+        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 p-2">
+            <div class="card card-custom h-100 bg-gradient-danger text-white">
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h5 class="mb-0 font-weight-bold">
+                                $ <span id="payable"></span>
+                            </h5>
+                            <p class="mb-0">Total Collection</p>
                         </div>
-                        <div class="col-3 col-lg-4 col-md-4 col-sm-3 text-end">
-                            <div class="icon icon-shape bg-gradient-primary shadow float-end border-radius-md">
-                                <img class="w-100 " src="{{asset('images/icon.svg')}}"/>
+                        <div class="col-4 text-right">
+                            <div class="icon-custom">
+                                <img class="w-100" src="{{asset('images/money.svg')}}" alt="Total Collection Icon"/>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 
